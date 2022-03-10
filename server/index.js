e3994692b5a24b931e4b84eb070ea3a7d2f3e347
Peter hp <phirschparker@gmail.com>
2022-03-10 19:13:58 +0100

    feat(index.js): add startServer func to initialise db before express server

--- server/index.js
+++ server/index.js
@@ -5,16 +5,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 // const { router } = require('./router');
-// const { startServer } = require('./db');
+const { startServer } = require('./db');
 
 app.use(cors());
 app.use(express.json());
 // app.use(router);
 
-// startServer()
-//   .then(
-//     app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT} 🤯`)),
-//   )
-//   .catch((e) => console.error(e));
-
-app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT} 🤯`));
+startServer()
+  .then(
+    app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT} 🤯`)),
+  )
+  .catch((e) => console.error(e));
