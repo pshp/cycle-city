34cad30d988f224f6836bd758905a1ffb1164ca5
Peter hp <phirschparker@gmail.com>
2022-03-10 18:38:25 +0100

    feat(index.js): created basic express server and installed packages

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+const cors = require('cors');
+//const { router } = require('./router');
+//const { startServer } = require('./db');
+
+app.use(cors());
+app.use(express.json());
+//app.use(router);
+
+// startServer()
+//   .then(
+//     app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT} 🤯`)),
+//   )
+//   .catch((e) => console.error(e));
+
+  app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT} 🤯`))
