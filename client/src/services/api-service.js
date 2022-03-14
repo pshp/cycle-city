e73ef0ee304d4c1cf06e32eecf76471e497d1595
Peter hp <phirschparker@gmail.com>
2022-03-14 10:02:40 +0100

    feat(api-service): created front-end api-service

---
+++ client/src/services/api-service.js
@@ -0,0 +1,48 @@
+const baseUrl = 'http://localhost:3001/';
+const headers = { 'Content-Type': 'application/json' };
+
+export const getLocations = async () => fetch(`${baseUrl}locations`, {
+  method: 'GET',
+  headers,
+})
+  .then((res) => res.json())
+  .catch((error) => console.log(error));
+
+export const postLocation = async (data) => fetch(`${baseUrl}locations`, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify(data),
+})
+  .then((res) => res.json())
+  .catch((error) => console.log(error));
+
+export const deleteLocation = async (id) => fetch(`${baseUrl}locations/${id}`, {
+  method: 'DELETE',
+  headers,
+})
+  .then((res) => res.json())
+  .catch((error) => console.log(error));
+
+export const editLocations = async (id, data) => fetch(`${baseUrl}locations/${id}`, {
+  method: 'PATCH',
+  headers,
+  body: JSON.stringify(data),
+})
+  .then((res) => res.json())
+  .catch((error) => console.log(error));
+
+export const registerUser = async (data) => fetch(`${baseUrl}users/register`, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify(data),
+})
+  .then((res) => res.json())
+  .catch((error) => console.log(error));
+
+export const loginUser = async (data) => fetch(`${baseUrl}users/login`, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify(data),
+})
+  .then((res) => res.json())
+  .catch((error) => console.log(error));
