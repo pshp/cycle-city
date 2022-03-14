3994b53b02b3a73e2c2ce86523742b42dcba9152
Peter hp <phirschparker@gmail.com>
2022-03-14 10:37:01 +0100

    feat(register page): created register form

--- client/src/componants/Register/Register.jsx
+++ client/src/componants/Register/Register.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import './Register.css';
+import { Link } from 'react-router-dom';
 
 export default function Register() {
+  const handleSumbit = (e) => {
+    e.preventDeafault();
+  };
+
   return (
     <div className="Register">
-      Register here
+      <h1>Cycle City</h1>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSumbit} className="signup">
+        <input type="text" />
+        <p>First name *</p>
+        <input type="text" />
+        <p>Last Name *</p>
+        <input type="text" />
+        <p>Email *</p>
+        <input type="text" />
+        <p>Password *</p>
+        <input type="text" />
+        <button className="sumbit-btn" type="submit">Sign Up</button>
+      </form>
+      Already have an account?
+      {' '}
+      <span><Link to="/login">Log in</Link></span>
     </div>
   );
 }
