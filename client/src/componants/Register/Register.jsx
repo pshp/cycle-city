73c5b62ba336a571735fed1648602db32f266e01
Peter hp <phirschparker@gmail.com>
2022-03-14 11:33:55 +0100

    refactor(register.css): changed class names

--- client/src/componants/Register/Register.jsx
+++ client/src/componants/Register/Register.jsx
@@ -13,13 +13,13 @@ export default function Register() {
       <form onSubmit={handleSumbit} className="user-form signup-form">
         <h2>Sign Up</h2>
         <p>First name *</p>
-        <input type="text" />
+        <input className="user-input" type="text" />
         <p>Last Name *</p>
-        <input type="text" />
+        <input className="user-input" type="text" />
         <p>Email *</p>
-        <input type="text" />
+        <input className="user-input" type="text" />
         <p>Password *</p>
-        <input type="text" />
+        <input className="user-input" type="text" />
         <button className="submit-btn" type="submit">Sign Up</button>
       </form>
       Already have an account?
