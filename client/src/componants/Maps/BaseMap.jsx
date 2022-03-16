83302023e581138c93aeadaa104120fe31423c83
Peter hp <phirschparker@gmail.com>
2022-03-16 14:23:56 +0100

    feat(pin marker): added pin marker with svg

--- client/src/componants/Maps/BaseMap.jsx
+++ client/src/componants/Maps/BaseMap.jsx
@@ -6,20 +6,24 @@ import {
 import 'leaflet/dist/leaflet.css';
 import Room from '@mui/icons-material/Room';
 import L from 'leaflet';
-import { SvgIcon } from '@mui/material';
 
 export default function BaseMap() {
-  const position = [51.505, -0.09];
+  const position = [52.51051,13.46104];
 
-  // const myIcon = L.icon({
-  //   icon: Room,
-  //   iconSize: [64, 64],
-  //   // iconAnchor: [32, 64],
-  //   // popupAnchor: null,
-  //   // shadowUrl: null,
-  //   // shadowSize: null,
-  //   // shadowAnchor: null
-  // });
+  const myIcon = L.divIcon({
+    iconSize: [26, 26],
+    iconAnchor: [13, 26],
+    popupAnchor: [0, -5],
+    html: `<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+       viewBox="0 0 492.452 492.452" style="enable-background:new 0 0 492.452 492.452;" xml:space="preserve">
+    <path id="XMLID_152_" d="M246.181,0C127.095,0,59.533,102.676,84.72,211.82c17.938,77.722,126.259,280.631,161.462,280.631
+      c32.892,0,143.243-202.975,161.463-280.631C432.996,103.74,365.965,0,246.181,0z M246.232,224.97
+      c-34.38,0-62.244-27.863-62.244-62.244c0-34.381,27.864-62.244,62.244-62.244c34.38,0,62.244,27.863,62.244,62.244
+      C308.476,197.107,280.612,224.97,246.232,224.97z"/>
+    </svg>`
+    ,
+    className: 'leaflet-data-marker',
+  });
 
   return (
     <MapContainer className="map-container " center={position} zoom={13} scrollWheelZoom={false}>
@@ -27,7 +31,7 @@ export default function BaseMap() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Room position={position}>
+      <Marker icon={myIcon} position={position}>
         <Popup>
 
           A pretty CSS3 popup.
@@ -36,7 +40,7 @@ export default function BaseMap() {
           {' '}
           Easily customizable.
         </Popup>
-      </Room>
+      </Marker>
     </MapContainer>
 
   );
